c814d773ab3002240e95346b14de2c924d3922fc
Serwy10 <elserwymohamed77@gmail.com>
2025-10-21 12:55:37 +0200

    Refactor main loop and clean up output
    Removed unnecessary output statements and adjusted loop structure.

--- BankSystem(OOP).cpp
+++ BankSystem(OOP).cpp
@@ -6,26 +6,16 @@ using namespace std;
 
 int main() {
 
-	/*while (true) {
+	while (true) {
 
 		if (!clsLogin::showLoginScreen())
 			break;
 
 		
-	}*/
+	}
 
 	
-	cout << char((int)'2'+6) << endl;
-	cout << char(3) << endl;
-	cout << char(4) << endl;
-	cout << char(5) << endl;
-	cout << char(6) << endl;
-	cout << char(7) << endl;
-	cout << char(8) << endl;
-	cout << char(9) << endl;
-	cout << char(10) << endl;
-	cout << char(11) << endl;
-	cout << char(12) << endl;
+	
 	
 	system("pause>0");
 }
